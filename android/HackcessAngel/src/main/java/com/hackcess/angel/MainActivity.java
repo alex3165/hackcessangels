d308a82355ac3aac2318a1f21ca7407bdc3205e3
Olivier Corradi <olivier.corradi@gmail.com>
2013-11-16 13:14:44 +0100

    Initial app commit

--- android/HackcessAngel/src/main/java/com/hackcess/angel/MainActivity.java
+++ android/HackcessAngel/src/main/java/com/hackcess/angel/MainActivity.java
@@ -1,17 +1,27 @@
 package com.hackcess.angel;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.os.Handler;
 
 public class MainActivity extends ActionBarActivity {
+
+    public final String TAG = "MainActivity";
+    private int REQUEST_ENABLE_BT = 1;
+    private Handler mHandler;
+    private BluetoothService bluetoothService;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +33,61 @@ public class MainActivity extends ActionBarActivity {
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
+
+        mHandler = new Handler();
+        bluetoothService = new BluetoothService(mHandler);
+
+        // Register a receiver for when a new bt device has been found during discovery
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(bluetoothService.mReceiver, filter);
+
+        // Register for broadcasts when discovery has finished
+        filter = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        registerReceiver(bluetoothService.mReceiver, filter);
+
+        // Get the default bluetooth adapter
+        if (bluetoothService.mBluetoothAdapter != null) {
+            // Device supports Bluetooth: check if it is enabled
+            if (!bluetoothService.mBluetoothAdapter.isEnabled()) {
+                // Bluetooth is not enabled: start it
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            } else {
+                // BT is already on, let's start listening immediately
+                ensureDiscoverable();
+                bluetoothService.startListening();
+            }
+        }
+    }
+
+    // Starts a thread which keeps the device discoverable
+    private final void ensureDiscoverable() {
+        if (bluetoothService.mBluetoothAdapter.getScanMode() !=
+                BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE) {
+            Intent discoverableIntent = new
+                    Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+            discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 0);
+            startActivity(discoverableIntent);
+            Log.d(TAG, "Forced discoverability");
+        } else {
+            Log.d(TAG, "Device is already discoverable");
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        if (requestCode == REQUEST_ENABLE_BT) {
+            if (resultCode == RESULT_OK) {
+                // Bluetooth is ready, start listening
+                ensureDiscoverable();
+                bluetoothService.startListening();
+            }
+        }
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
