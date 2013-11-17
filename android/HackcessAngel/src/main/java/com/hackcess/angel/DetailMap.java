a0ba0e954c93b5f4fe4e9873efc146dd24bc5d95
Etienne J. Membrives <etienne@membrives.fr>
2013-11-17 13:02:15 +0100

    Zoomed map

--- android/HackcessAngel/src/main/java/com/hackcess/angel/DetailMap.java
+++ android/HackcessAngel/src/main/java/com/hackcess/angel/DetailMap.java
@@ -22,11 +22,14 @@ public class DetailMap extends Fragment {
     private MapView myOpenMapView;
     private IMapController myMapController;
 
-    public static final OnlineTileSourceBase MAPNIK = new XYTileSource("Mapnik",
+    public static final OnlineTileSourceBase OSMFR = new XYTileSource("Mapnik",
             ResourceProxy.string.mapnik, 0, 20, TILE_SIZE, ".png",
             "http://a.tile.openstreetmap.fr/osmfr/",
             "http://b.tile.openstreetmap.fr/osmfr/",
             "http://c.tile.openstreetmap.fr/osmfr/");
+
+    public static final OnlineTileSourceBase MAPNIK = new XYTileSource("Mapnik",
+            ResourceProxy.string.mapnik, 0, 18, 512, ".png", "http://tile.openstreetmap.org/");
 
     /*private abstract class Worker extends AsyncTask<URL, Void, Response> {
         AppEngineClient client;
@@ -63,40 +66,37 @@ public class DetailMap extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.map_detail, container, false);
 
-        ImageButton Info=(ImageButton) rootView.findViewById(R.id.buttonInfo);
-        ImageButton Chair=(ImageButton) rootView.findViewById(R.id.buttonChair);
-        ImageButton Blind=(ImageButton) rootView.findViewById(R.id.buttonBlind);
-        ImageButton Move=(ImageButton) rootView.findViewById(R.id.buttonMove);
-        ImageButton Deaf=(ImageButton) rootView.findViewById(R.id.buttonDeaf);
-
-        // Create a custom tile source
-        final ITileSource tileSource = MAPNIK;
+        ImageButton Info = (ImageButton) rootView.findViewById(R.id.buttonInfo);
+        ImageButton Chair = (ImageButton) rootView.findViewById(R.id.buttonChair);
+        ImageButton Blind = (ImageButton) rootView.findViewById(R.id.buttonBlind);
+        ImageButton Move = (ImageButton) rootView.findViewById(R.id.buttonMove);
+        ImageButton Deaf = (ImageButton) rootView.findViewById(R.id.buttonDeaf);
 
         // Create the mapview with the custom tile provider array
         myOpenMapView = (MapView) rootView.findViewById(R.id.openmapview);
-        myOpenMapView.setTileSource(tileSource);
+        myOpenMapView.setTileSource(MAPNIK);
 
         myMapController = myOpenMapView.getController();
-        myMapController.setZoom(14);
+        myMapController.setZoom(17);
         myMapController.setCenter(new GeoPoint(48.843906, 2.375278));
 
-        if (Info.isSelected()){
+        if (Info.isSelected()) {
             //Affiche les infos générales sur la map
 
         }
-        if (Chair.isSelected()){
+        if (Chair.isSelected()) {
             //Affiche les accès fauteuils roulants
 
         }
-        if (Blind.isSelected()){
+        if (Blind.isSelected()) {
             //Affiche les infos pour les malvoyants
 
         }
-        if (Move.isSelected()){
+        if (Move.isSelected()) {
             //Affiche les accès
 
         }
-        if (Deaf.isSelected()){
+        if (Deaf.isSelected()) {
             //Affiche les infos audio
 
         }
@@ -104,7 +104,7 @@ public class DetailMap extends Fragment {
     }
 
     public void onActivityResult(int requestCode, int resultCode,
-                                    Intent data) {
+                                 Intent data) {
         android.util.Log.d("TEST", "truc");
     }
 
