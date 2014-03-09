2caef1f6c6a0f4ea9000ade9aa340e7a9a45fe3a
Etienne J. Membrives <etienne@membrives.fr>
2014-03-09 18:17:12 +0100

    Removing old server code

--- server/runserver.py
+++
@@ -1,2 +0,0 @@
-from server import app
-app.run("127.0.0.1", debug=True)
--- server/server/error_messages.py
+++
@@ -1,5 +0,0 @@
-# coding=utf-8
-
-USER_ALREADY_EXISTING = "Cet utilisateur existe déjà."
-UNKNOWN_USER = "Cet utilisateur est inconnu."
-MALFORMED_REQUEST = "La requête est invalide."
