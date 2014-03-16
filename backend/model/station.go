438a7fce342ecd6b39bb4dc32307a6ef6d4c30cc
Etienne J. Membrives <etienne@membrives.fr>
2014-03-16 20:06:47 +0100

    Stations, part 1

--- backend/model/station.go
+++ backend/model/station.go
@@ -1,5 +1,52 @@
 package model
 
+import (
+    "os"
+    "encoding/csv"
+
+	"labix.org/v2/mgo/bson"
+)
+
+const (
+    stationCoordinates = "data/stations.csv"
+)
+
+type PolygonGeometry struct {
+	Type        string        `bson:"type"`
+	Coordinates [][][]float64 `bson:"coordinates"`
+}
+
+func NewPolygonGeometry(points [][]float64) *PolygonGeometry {
+	p := &PolygonGeometry{
+		Type:        "polygon",
+		Coordinates: [][][]float64{points},
+	}
+	return p
+}
+
 type Station struct {
-	m                     *Model `bson:"-"`
+	Id   bson.ObjectId `bson:"_id,omitempty"`
+	Name string
+
+	Center  *PointGeometry
+	Outline *PolygonGeometry
+
+	m *Model `bson:"-"`
+}
+
+func (m *Model) ResetAndLoadStationsFromFile() error {
+    m.stations.RemoveAll(bson.M{})
+    
+    r := csv.NewReader(os.Open(stationCoordinates))
+    r.Comma = ';'
+    r.LazyQuotes = true
+    r.TrimLeadingSpace = true
+
+    header, err := r.Read()
+    if err != nil {
+        return err
+    }
+    for  {
+    }
+
 }
