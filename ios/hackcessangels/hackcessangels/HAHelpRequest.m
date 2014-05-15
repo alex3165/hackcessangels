6081b39198d8dc20b6ac6146e596934a975565f6
Etienne J. Membrives <etienne@membrives.fr>
2014-05-15 21:33:33 +0200

    HAHelpRequest: initWithDictionary init method to create object from JSON response

--- ios/hackcessangels/hackcessangels/HAHelpRequest.m
+++ ios/hackcessangels/hackcessangels/HAHelpRequest.m
@@ -8,6 +8,33 @@
 
 #import "HAHelpRequest.h"
 
+NSString *const kIdKey = @"Id";
+NSString *const kLatitudeKey = @"Latitude";
+NSString *const kLongitudeKey = @"Longitude";
+NSString *const kPrecisionKey = @"Precision";
+NSString *const kAgentKey = @"Agent";
+NSString *const kUserKey = @"User";
+NSString *const kStatusKey = @"CurrentState";
+
 @implementation HAHelpRequest
+
+- (id)initWithDictionary:(NSDictionary *)data {
+    self = [super init];
+    
+    if (self) {
+        self.Id = [data objectForKey:kIdKey];
+        self.latitude = [[data objectForKey:kLatitudeKey] doubleValue];
+        self.longitude = [[data objectForKey:kLongitudeKey] doubleValue];
+        self.precision = [[data objectForKey:kPrecisionKey] doubleValue];
+        if ([data objectForKey:kAgentKey]) {
+            self.agent = [[HAAgent alloc] initWithDictionary:[data objectForKey:kAgentKey]];
+        }
+        if ([data objectForKey:kUserKey]) {
+            self.user = [[HAUser alloc] initWithDictionary:[data objectForKey:kUserKey]];
+        }
+        self.status = [[data objectForKey:kStatusKey] integerValue];
+    }
+    return self;
+}
 
 @end
