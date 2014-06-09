f0312ad6c905157c388a0cd3c0b172784926726e
Etienne J. Membrives <etienne@membrives.fr>
2014-06-09 11:03:40 +0200

    Stable secrets for cookie generation

--- backend/main/main.go
+++ backend/main/main.go
@@ -9,7 +9,9 @@ import (
 
 func main() {
 	flag.Parse()
-	server := server.NewServer()
+	server := server.NewServerWithCookieStore(
+        []byte(";X,\"ky_nRYY>R~pl{:/<w_Xw$h92YwxmC*#^bZJEttI#*J1LuJtvAGkLDJyCJkr5"),
+        []byte("LS<5WvNp>s+}Fr<L:Ja=E(&Q1\"SixzIl"))
 	log.Fatal(server.Run())
 
 }
