4d054295f91912043f64d9da4fe3f77ca828429a
Etienne J. Membrives <etienne@membrives.fr>
2014-07-04 10:57:08 +0200

    Tool to reset a password manually

---
+++ backend/scripts/reset/reset.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+    "flag"
+    "log"
+
+    "hackcessangels/backend/model"
+)
+
+var (
+    account = flag.String("account", "", "login of the account")
+    password = flag.String("password", "", "password to set on the account")
+)
+
+func main() {
+    flag.Parse()
+
+    if len(*password) == 0 {
+        log.Fatal("password must not be empty")
+    }
+
+    m, err := model.NewModel("localhost", "hackcessangels")
+    if err != nil {
+        log.Fatal("Unable to create model: ", err)
+    }
+
+    user, err := m.GetUserByEmail(*account)
+    if err != nil {
+        log.Fatal("Unable to get user: ",err)
+    }
+
+    if err = user.SetPassword(*password); err != nil {
+        log.Fatal("Unable to set password: ", err)
+    }
+
+    if err = user.Save(); err != nil {
+        log.Fatal("Unable to save modified user: ", err)
+    }
+}
