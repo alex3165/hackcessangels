00ca8a2022c6d708905af5d31b0b690595d55da2
Etienne J. Membrives <etienne@membrives.fr>
2014-07-05 18:36:41 +0200

    Send station name to agent

--- backend/main/main.go
+++ backend/main/main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	flag.Parse()
 	server := server.NewServerWithCookieStore(
-        []byte(";X,\"ky_nRYY>R~pl{:/<w_Xw$h92YwxmC*#^bZJEttI#*J1LuJtvAGkLDJyCJkr5"),
-        []byte("LS<5WvNp>s+}Fr<L:Ja=E(&Q1\"SixzIl"))
+		[]byte(";X,\"ky_nRYY>R~pl{:/<w_Xw$h92YwxmC*#^bZJEttI#*J1LuJtvAGkLDJyCJkr5"),
+		[]byte("LS<5WvNp>s+}Fr<L:Ja=E(&Q1\"SixzIl"))
 	log.Fatal(server.Run())
 }
